74c9805b89e5def1246b591ab56cc70735335831
rogur04 <neigmer_14@hotmail.com>
2019-01-14 19:32:18 +0100

    aaaaaabbbbbbb

--- decide/local_settings.travis.py
+++ decide/local_settings.travis.py
@@ -30,6 +30,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+	'PASSWORD':'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
